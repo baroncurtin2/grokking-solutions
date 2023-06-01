bec3215e5202612a7cbf40221f34eec4941f0d5f
baroncurtin2 <knowledge3@gmail.com>
2023-06-01 19:41:06 -0400

    Squashed 'python/' changes from bd9deaa..e52d464
    e52d464 python contains duplicate
    c2e5561 ignore
    ab4b072 fix test
    0b93f4f python contains duplicate
    da85f9d setup
    
    git-subtree-dir: python
    git-subtree-split: e52d464d1bbeee107c75d0acc8dddcacc53dbd35

---
+++ grokking/warmup/contains_duplicate.py
@@ -0,0 +1,31 @@
+def contains_duplicate_brute_force(nums: list[int]) -> bool:
+    # time: O(n^2)
+    # space: O(1)
+
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] == nums[j]:
+                return True
+
+    return False
+
+
+def contains_duplicate_set(nums: list[int]) -> bool:
+    # time: O(n)
+    # space: O(n)
+    uniques = set()
+
+    for num in nums:
+        if num in uniques:
+            return True
+
+        uniques.add(num)
+    return False
+
+
+def contains_duplicate_sorting(nums: list[int]) -> bool:
+    # time: O(nlogn)
+    # space: O(n)
+    nums = sorted(nums)
+
+    return any(nums[i] == nums[i + 1] for i in range(len(nums) - 1))

f615e3cad3d591c192849fd33f21e4951876684b
baroncurtin2 <knowledge3@gmail.com>
2023-06-01 21:55:11 -0400

    javascript contains duplicate

---
+++ javascript/tests/warmup/containsDuplicate.test.js
@@ -0,0 +1,22 @@
+const { containsDuplicateBruteForce, containsDuplicateSet, containsDuplicateSorting } = require('../../src/warmup/containsDuplicate');
+
+describe('containsDuplicate.js', () => {
+    const tests = [
+        {
+            nums: [1, 2, 3, 1],
+            expected: true,
+        },
+        {
+            nums: [3, 9, 1, 2],
+            expected: false,
+        },
+    ];
+
+    tests.forEach((test, idx) => {
+        it(`testCase: ${idx}`, () => {
+            expect(containsDuplicateBruteForce(test.nums)).toBe(test.expected);
+            expect(containsDuplicateSet(test.nums)).toBe(test.expected);
+            expect(containsDuplicateSorting(test.nums)).toBe(test.expected);
+        })
+    });
+});

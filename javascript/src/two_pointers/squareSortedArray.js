f38ba9e76de8155b43ea3190ff57fba117fb83f5
baroncurtin2 <knowledge3@gmail.com>
2023-06-09 22:46:07 -0400

    javascript square sorted array

---
+++ javascript/src/two_pointers/squareSortedArray.js
@@ -0,0 +1,23 @@
+export function makeSquares(arr) {
+    const n = arr.length;
+    let highSqIdx = n - 1;
+    let squares = Array(n).fill(0);
+
+    let left = 0, right = n - 1;
+
+    while (left <= right) {
+        const leftSq = arr[left] * arr[left], rightSq = arr[right] * arr[right];
+
+        if (leftSq > rightSq) {
+            squares[highSqIdx] = leftSq;
+            left++;
+        } else {
+            squares[highSqIdx] = rightSq;
+            right--;
+        }
+
+        highSqIdx--;
+    }
+
+    return squares;
+}

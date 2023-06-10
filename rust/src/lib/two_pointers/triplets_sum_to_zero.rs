22602dfc411f175a8c3eb060402b5bdc202fe768
baroncurtin2 <knowledge3@gmail.com>
2023-06-10 15:25:21 -0400

    rust triplet sum to zero

---
+++ rust/src/lib/two_pointers/triplets_sum_to_zero.rs
@@ -0,0 +1,77 @@
+fn search_triplets(arr: &mut [i32]) -> Vec<[i32; 3]> {
+    let mut triplets: Vec<[i32; 3]> = Vec::new();
+    arr.sort();
+
+    let mut i = 0;
+    while i < arr.len() {
+        if i > 0 && arr[i] == arr[i - 1] {
+            i += 1;
+            continue;
+        }
+
+        search_pair(arr, -arr[i], i + 1, &mut triplets);
+        i += 1
+    }
+
+    triplets
+}
+
+fn search_pair(arr: &[i32], target: i32, mut left: usize, triplets: &mut Vec<[i32; 3]>) {
+    let mut right = arr.len() - 1;
+
+    while left < right {
+        let total = arr[left] + arr[right];
+
+        if total < target {
+            left += 1;
+        } else if total > target {
+            right -= 1;
+        } else {
+            triplets.push([-target, arr[left], arr[right]]);
+            left += 1;
+            right -= 1;
+
+            while left < right && arr[left] == arr[left - 1] {
+                left += 1;
+            }
+
+            while left < right && arr[right] == arr[right + 1] {
+                right -= 1;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_eq::assert_eq;
+
+    #[derive(Debug, PartialEq)]
+    struct TestCase {
+        arr: Vec<i32>,
+        expected: Vec<[i32; 3]>,
+    }
+
+    #[test]
+    fn test_search_triplets() {
+        let tests = vec![
+            TestCase {
+                arr: vec![-3, 0, 1, 2, -1, 1, -2],
+                expected: vec![[-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]],
+            },
+            TestCase {
+                arr: vec![-5, 2, -1, -2, 3],
+                expected: vec![[-5, 2, 3], [-2, -1, 3]],
+            },
+        ];
+
+        for (idx, test) in tests.iter().enumerate() {
+            let result = search_triplets(&test.arr);
+            assert_eq!(test.expected.len(), result.len());
+            for expected in &test.expected {
+                assert!(result.contains(expected));
+            }
+        }
+    }
+}

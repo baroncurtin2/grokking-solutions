c1e5df81ac2897c68b2c4f07978339b68f58dc70
baroncurtin2 <knowledge3@gmail.com>
2023-06-09 23:39:21 -0400

    rust square sorted array

--- rust/src/lib/two_pointers/mod.rs
+++ rust/src/lib/two_pointers/mod.rs
@@ -1,2 +1,3 @@
 pub mod pair_with_target;
 pub mod remove_duplicates;
+pub mod square_sorted_array;
---
+++ rust/src/lib/two_pointers/square_sorted_array.rs
@@ -0,0 +1,43 @@
+fn make_squares(arr: &[i32]) -> Vec<i32> {
+    if arr.is_empty() {
+        return Vec::new();
+    }
+
+    let n = arr.len();
+    let mut high_sq_idx = n - 1;
+    let mut squares = vec![0; n];
+
+    let (mut left, mut right) = (0, n - 1);
+
+    for high_sq_idx in (0..n).rev() {
+        let left_sq = arr[left] * arr[left];
+        let right_sq = arr[right] * arr[right];
+
+        if left_sq > right_sq {
+            squares[high_sq_idx] = left_sq;
+            left += 1;
+        } else {
+            squares[high_sq_idx] = right_sq;
+            right -= 1;
+        }
+    }
+
+    squares
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_make_squares() {
+        let test_cases = [
+            (&[-2, -1, 0, 2, 3], &[0, 1, 4, 4, 9]),
+            (&[-3, -1, 0, 1, 2], &[0, 1, 1, 4, 9]),
+        ];
+
+        for (arr, expected) in test_cases {
+            assert_eq!(make_squares(arr), expected);
+        }
+    }
+}
